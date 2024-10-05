7f7c9ea19d16413029750a6b5b7cc501a5717989
Jonathan Perry <jpper@umich.edu>
2024-10-05 09:43:03 -0500

    Show toast messages

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import emailjs from "@emailjs/browser";
+import { toast, Toaster } from "sonner";
 
 export default function Form() {
   const {
@@ -11,6 +12,8 @@ export default function Form() {
   } = useForm();
 
   const sendEmail = (params) => {
+    const toastId = toast.loading("Sending your message, please wait...");
+
     emailjs
       .send(
         process.env.NEXT_PUBLIC_SERVICE_ID,
@@ -19,16 +22,27 @@ export default function Form() {
         {
           publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
           limitRate: {
-            throttle: 5000, // Cannot send more than 1 email/5 sec
+            // Cannot send more than 1 email/5 sec
+            throttle: 5000,
           },
         }
       )
       .then(
         () => {
-          console.log("SUCCESS!");
+          toast.success(
+            "I have received your message. I will get back to you soon.",
+            {
+              id: toastId,
+            }
+          );
         },
         (error) => {
-          console.log("FAILED...", error.text);
+          toast.error(
+            "There was an error sending your message. Please try again later.",
+            {
+              id: toastId,
+            }
+          );
         }
       );
   };
@@ -45,69 +59,72 @@ export default function Form() {
   console.log(errors);
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
-    >
-      {/* Name input */}
-      <input
-        className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-        type="text"
-        placeholder="Name"
-        {...register("name", {
-          required: "This field is required.",
-          minLength: {
-            value: 3,
-            message: "Name should be at least 3 characters long.",
-          },
-        })}
-      />
-      {errors.name && (
-        <span className="inline-block self-start text-accent">
-          {errors.name.message}
-        </span>
-      )}
+    <>
+      <Toaster richColors={true} />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
+      >
+        {/* Name input */}
+        <input
+          className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+          type="text"
+          placeholder="Name"
+          {...register("name", {
+            required: "This field is required.",
+            minLength: {
+              value: 3,
+              message: "Name should be at least 3 characters long.",
+            },
+          })}
+        />
+        {errors.name && (
+          <span className="inline-block self-start text-accent">
+            {errors.name.message}
+          </span>
+        )}
 
-      {/* Email input */}
-      <input
-        className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-        type="email"
-        placeholder="Email"
-        {...register("email", { required: "This field is required." })}
-      />
-      {errors.email && (
-        <span className="inline-block self-start text-accent">
-          {errors.email.message}
-        </span>
-      )}
+        {/* Email input */}
+        <input
+          className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+          type="email"
+          placeholder="Email"
+          {...register("email", { required: "This field is required." })}
+        />
+        {errors.email && (
+          <span className="inline-block self-start text-accent">
+            {errors.email.message}
+          </span>
+        )}
 
-      {/* Message input */}
-      <textarea
-        placeholder="Message"
-        className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-        {...register("message", {
-          required: "This field is required.",
-          maxLength: {
-            value: 500,
-            message: "Message should be less than 500 characters.",
-          },
-          minLength: {
-            value: 50,
-            message: "Message should be more than 50 characters.",
-          },
-        })}
-      />
-      {errors.message && (
-        <span className="inline-block self-start text-accent">
-          {errors.message.message}
-        </span>
-      )}
+        {/* Message input */}
+        <textarea
+          placeholder="Message"
+          className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+          {...register("message", {
+            required: "This field is required.",
+            maxLength: {
+              value: 500,
+              message: "Message should be less than 500 characters.",
+            },
+            minLength: {
+              value: 50,
+              message: "Message should be more than 50 characters.",
+            },
+          })}
+        />
+        {errors.message && (
+          <span className="inline-block self-start text-accent">
+            {errors.message.message}
+          </span>
+        )}
 
-      <input
-        value="Cast your message!"
-        className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize"
-        type="submit"
-      />
-    </form>
+        <input
+          value="Cast your message!"
+          className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize"
+          type="submit"
+        />
+      </form>
+    </>
   );
 }
