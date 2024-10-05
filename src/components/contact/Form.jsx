7936b3edfb7c2dbb877da44d0a67ac482d007664
Jonathan Perry <jpper@umich.edu>
2024-10-05 09:21:30 -0500

    Use emailjs on contact page

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 
 export default function Form() {
   const {
@@ -8,7 +9,39 @@ export default function Form() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  const sendEmail = (params) => {
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        params,
+        {
+          publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+          limitRate: {
+            throttle: 5000, // Cannot send more than 1 email/5 sec
+          },
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
+  const onSubmit = (data) => {
+    const templateParams = {
+      to_name: "Jonathan Perry",
+      from_name: data.name,
+      reply_to: data.email,
+      message: data.message,
+    };
+    sendEmail(templateParams);
+  };
   console.log(errors);
 
   return (
@@ -16,23 +49,59 @@ export default function Form() {
       onSubmit={handleSubmit(onSubmit)}
       className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
     >
+      {/* Name input */}
       <input
         className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
         type="text"
         placeholder="Name"
-        {...register("name", { required: true })}
+        {...register("name", {
+          required: "This field is required.",
+          minLength: {
+            value: 3,
+            message: "Name should be at least 3 characters long.",
+          },
+        })}
       />
+      {errors.name && (
+        <span className="inline-block self-start text-accent">
+          {errors.name.message}
+        </span>
+      )}
+
+      {/* Email input */}
       <input
         className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
         type="email"
         placeholder="Email"
-        {...register("email", { required: true })}
+        {...register("email", { required: "This field is required." })}
       />
+      {errors.email && (
+        <span className="inline-block self-start text-accent">
+          {errors.email.message}
+        </span>
+      )}
+
+      {/* Message input */}
       <textarea
         placeholder="Message"
         className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
-        {...register("message", { required: true, max: 256, min: 50 })}
+        {...register("message", {
+          required: "This field is required.",
+          maxLength: {
+            value: 500,
+            message: "Message should be less than 500 characters.",
+          },
+          minLength: {
+            value: 50,
+            message: "Message should be more than 50 characters.",
+          },
+        })}
       />
+      {errors.message && (
+        <span className="inline-block self-start text-accent">
+          {errors.message.message}
+        </span>
+      )}
 
       <input
         value="Cast your message!"
