5834c9d1ec9ccb043017520a7bd0e12227dbfb91
Jonathan Perry <jpper@umich.edu>
2024-10-04 23:54:02 -0500

    Implement github stats and skill icons

--- src/components/HomeBtn.jsx
+++ src/components/HomeBtn.jsx
@@ -5,12 +5,13 @@ import React from "react";
 const HomeBtn = () => {
   return (
     <Link
-      href={"/"}
+      href="/"
       target={"_self"}
-      className="text-foreground group rounded-full flex items-center
-                 justify-center custom-bg fixed top-4 left-4 w-fit self-start"
+      className="text-foreground rounded-full flex items-center
+                 justify-center custom-bg fixed top-4 left-4 w-fit self-start z-50"
       aria-label={"home"}
       name={"home"}
+      prefetch={false}
     >
       <span className="relative w-14 h-14 p-4 hover:text-accent">
         <Home className="w-full h-auto" strokeWidth={1.5} />
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Link from "next/link";
 import React from "react";
 
 const ItemLayout = ({ children, className }) => {
@@ -34,15 +35,71 @@ const AboutDetails = () => {
             spells and technologies to shape the future of the web.
           </p>
         </ItemLayout>
+
         <ItemLayout className="col-span-4 text-accent">
           <p className="font-semibold w-full text-left text-5xl">
             25+ <sub className="font-semibold text-base">clients</sub>
           </p>
         </ItemLayout>
+
         <ItemLayout className="col-span-4 text-accent">
           <p className="font-semibold w-full text-left text-5xl">
-            7+ <sub className="font-semibold text-base">years of experience</sub>
+            7+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
           </p>
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-4 !p-0"}>
+          <img
+            className={"w-full h-auto"}
+            src="https://github-readme-stats.vercel.app/api/top-langs?username=jonathanpperry&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false"
+            alt="jonnyramen"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-8 !p-0"}>
+          <img
+            className={"w-full h-auto"}
+            src="https://github-readme-stats.vercel.app/api?username=jonathanpperry&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false"
+            alt="jonnyramen"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
+            alt="jonnyramen"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={
+              "https://github-readme-streak-stats.herokuapp.com?user=jonathanpperry&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B"
+            }
+            alt="jonnyramen"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/jonathanpperry/next-contentlayer-blog"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`https://github-readme-stats.vercel.app/api/pin/?username=jonathanpperry&repo=next-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="jonnyramen"
+              loading="lazy"
+            />
+          </Link>
         </ItemLayout>
       </div>
     </section>
